f75873c70ad92dad606887a8f95c8ed13b232c1d
gineude <gineude@gmail.com>
2023-10-30 15:34:55 -0300

    Rota para o componente TECNICO-CREATE criada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from "./auth/auth.guard";
 import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from "./components/login/login.component";
 import { NavComponent } from "./components/nav/nav.component";
+import { TecnicoCreateComponent } from "./components/tecnico/tecnico-create/tecnico-create.component";
 import { TecnicoListComponent } from "./components/tecnico/tecnico-list/tecnico-list.component";
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
 		children: [
 			{ path: "home", component: HomeComponent },
 			{ path: "tecnicos", component: TecnicoListComponent },
+			{ path: "tecnicos/create", component: TecnicoCreateComponent },
 		],
 	},
 ];
